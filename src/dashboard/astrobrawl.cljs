5031f0bf409aaed8aff46c82e6fae93787e9d26e
RichoM <richi.moran@gmail.com>
2024-12-30 11:06:05 -0300

    Removed unnecessary charts

--- src/dashboard/astrobrawl.cljs
+++ src/dashboard/astrobrawl.cljs
@@ -19,61 +19,12 @@
   (-> match meta :session :version parse-version))
 
 (defn astrobrawl [{:keys [matches]}]
-  (let [pre-2_1-matches (->> matches
-                             (filter (comp #{"AstroBrawl"} :game))
-                             (filter #(older-version? (match-version %) [2 1])))
-        post-2_1-matches (->> matches
-                              (filter (comp #{"AstroBrawl"} :game))
-                              (remove #(older-version? (match-version %) [2 1])))
-        pre-2_1-sessions (->> pre-2_1-matches
-                              (map (comp :session meta))
-                              (set))
-        post-2_1-sessions (->> post-2_1-matches
-                               (map (comp :session meta))
-                               (set))]
+  (let [matches (->> matches
+                     (filter (comp #{"AstroBrawl"} :game)))]    
     [:div.row.my-4
-     [:div.col-auto
-      (ui/title "Partidas")
-      (vega/bar :values [{:version "< 2.1" :count (count pre-2_1-matches)}
-                         {:version "> 2.1" :count (count post-2_1-matches)}]
-                :width 150
-                :height 256
-                :x {:field :version
-                    :title "Versión"
-                    :axis {:labelAngle 0}}
-                :y {:field :count
-                    :title "Cantidad"}
-                :color {:field :version})]
-
-     [:div.col-auto
-      (ui/title "Duración de las sesiones")
-      (vega/boxplot :values [(assoc (ui/boxplot-stats (map :duration_m pre-2_1-sessions))
-                                    :version "< 2.1")
-                             (assoc (ui/boxplot-stats (map :duration_m post-2_1-sessions))
-                                    :version "> 2.1")]
-                    :width 150
-                    :height 256
-                    :x {:field :version
-                        :title "Versión"}
-                    :y {:title "Duración (minutos)"}
-                    :color {:field :version})]
-
-     [:div.col-auto
-      (ui/title "Partidas por sesión (promedio)")
-      (vega/bar :values [{:version "< 2.1" :count (average (mapv :match_count pre-2_1-sessions))}
-                         {:version "> 2.1" :count (average (mapv :match_count post-2_1-sessions))}]
-                :width 150
-                :height 256
-                :x {:field :version
-                    :title "Versión"
-                    :axis {:labelAngle 0}}
-                :y {:field :count
-                    :title "Cantidad"}
-                :color {:field :version})]
      [:div.col-auto
       (ui/title "Waves")
       (vega/bar :values (->> matches
-                             (filter (comp #{"AstroBrawl"} :game))
                              (filter (comp #{"SURVIVAL"} :mode))
                              (keep :metadata)
                              (map :round_idx)
