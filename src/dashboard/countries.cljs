589916f9d20eae85c450a4a26b961f2a59531e31
RichoM <richi.moran@gmail.com>
2025-02-03 12:13:00 -0300

    Added estimated timezones

--- src/dashboard/countries.cljs
+++ src/dashboard/countries.cljs
@@ -1,6 +1,6 @@
 (ns dashboard.countries)
 
-(defrecord Country [id code name])
+(defrecord Country [id code name delta-s])
 
 (def ^:private country-id {"AF" 4
                            "AX" 248
@@ -252,6 +252,8 @@
                            "ZM" 894
                            "ZW" 716})
 
+(count country-id)
+
 (def ^:private country-name {"AF" "Afghanistan"
                              "AX" "Åland Islands"
                              "AL" "Albania"
@@ -502,11 +504,262 @@
                              "ZM" "Zambia"
                              "ZW" "Zimbabwe"})
 
+(def ^private country-tz {"EH" (* 1 60 60)
+                          "BV" (* 1 60 60)
+                          "HM" (* 5 60 60)
+                          "AF" 16200
+                          "AL" 3600
+                          "DZ" 3600
+                          "AS" -39600
+                          "AD" 3600
+                          "AO" 3600
+                          "AI" -14400
+                          "AQ" 0
+                          "AG" -14400
+                          "AR" -10800
+                          "AM" 14400
+                          "AW" -14400
+                          "AU" 39600
+                          "AT" 3600
+                          "AZ" 14400
+                          "BS" -18000
+                          "BH" 10800
+                          "BD" 21600
+                          "BB" -14400
+                          "BY" 10800
+                          "BE" 3600
+                          "BZ" -21600
+                          "BJ" 3600
+                          "BM" -14400
+                          "BT" 21600
+                          "BO" -14400
+                          "BQ" -14400
+                          "BA" 3600
+                          "BW" 7200
+                          "BR" -10800
+                          "IO" 21600
+                          "BN" 28800
+                          "BG" 7200
+                          "BF" 0
+                          "BI" 7200
+                          "KH" 25200
+                          "CM" 3600
+                          "CA" -18000
+                          "CV" -3600
+                          "KY" -18000
+                          "CF" 3600
+                          "TD" 3600
+                          "CL" -10800
+                          "CN" 28800
+                          "CX" 25200
+                          "CC" 23400
+                          "CO" -18000
+                          "KM" 10800
+                          "CG" 3600
+                          "CD" 3600
+                          "CK" -36000
+                          "CR" -21600
+                          "HR" 3600
+                          "CU" -18000
+                          "CW" -14400
+                          "CY" 7200
+                          "CZ" 3600
+                          "CI" 0
+                          "DK" 3600
+                          "DJ" 10800
+                          "DM" -14400
+                          "DO" -14400
+                          "EC" -18000
+                          "EG" 7200
+                          "SV" -21600
+                          "GQ" 3600
+                          "ER" 10800
+                          "EE" 7200
+                          "ET" 10800
+                          "FK" -10800
+                          "FO" 0
+                          "FJ" 43200
+                          "FI" 7200
+                          "FR" 3600
+                          "GF" -10800
+                          "PF" -36000
+                          "TF" 18000
+                          "GA" 3600
+                          "GM" 0
+                          "GE" 14400
+                          "DE" 3600
+                          "GH" 0
+                          "GI" 3600
+                          "GR" 7200
+                          "GL" -7200
+                          "GD" -14400
+                          "GP" -14400
+                          "GU" 36000
+                          "GT" -21600
+                          "GG" 0
+                          "GN" 0
+                          "GW" 0
+                          "GY" -14400
+                          "HT" -18000
+                          "VA" 3600
+                          "HN" -21600
+                          "HK" 28800
+                          "HU" 3600
+                          "IS" 0
+                          "IN" 19800
+                          "ID" 25200
+                          "IR" 12600
+                          "IQ" 10800
+                          "IE" 0
+                          "IM" 0
+                          "IL" 7200
+                          "IT" 3600
+                          "JM" -18000
+                          "JP" 32400
+                          "JE" 0
+                          "JO" 10800
+                          "KZ" 18000
+                          "KE" 10800
+                          "KI" 50400
+                          "KP" 32400
+                          "KR" 32400
+                          "KW" 10800
+                          "KG" 21600
+                          "LA" 25200
+                          "LV" 7200
+                          "LB" 7200
+                          "LS" 7200
+                          "LR" 0
+                          "LY" 7200
+                          "LI" 3600
+                          "LT" 7200
+                          "LU" 3600
+                          "MO" 28800
+                          "MK" 3600
+                          "MG" 10800
+                          "MW" 7200
+                          "MY" 28800
+                          "MV" 18000
+                          "ML" 0
+                          "MT" 3600
+                          "MH" 43200
+                          "MQ" -14400
+                          "MR" 0
+                          "MU" 14400
+                          "YT" 10800
+                          "MX" -21600
+                          "FM" 39600
+                          "MD" 7200
+                          "MC" 3600
+                          "MN" 28800
+                          "ME" 3600
+                          "MS" -14400
+                          "MA" 3600
+                          "MZ" 7200
+                          "MM" 23400
+                          "NA" 7200
+                          "NR" 43200
+                          "NP" 20700
+                          "NL" 3600
+                          "NC" 39600
+                          "NZ" 46800
+                          "NI" -21600
+                          "NE" 3600
+                          "NG" 3600
+                          "NU" -39600
+                          "NF" 43200
+                          "MP" 36000
+                          "NO" 3600
+                          "OM" 14400
+                          "PK" 18000
+                          "PW" 32400
+                          "PS" 7200
+                          "PA" -18000
+                          "PG" 36000
+                          "PY" -10800
+                          "PE" -18000
+                          "PH" 28800
+                          "PN" -28800
+                          "PL" 3600
+                          "PT" 0
+                          "PR" -14400
+                          "QA" 10800
+                          "RO" 7200
+                          "RU" 10800
+                          "RW" 7200
+                          "RE" 14400
+                          "BL" -14400
+                          "SH" 0
+                          "KN" -14400
+                          "LC" -14400
+                          "MF" -14400
+                          "PM" -10800
+                          "VC" -14400
+                          "WS" 46800
+                          "SM" 3600
+                          "ST" 0
+                          "SA" 10800
+                          "SN" 0
+                          "RS" 3600
+                          "SC" 14400
+                          "SL" 0
+                          "SG" 28800
+                          "SX" -14400
+                          "SK" 3600
+                          "SI" 3600
+                          "SB" 39600
+                          "SO" 10800
+                          "ZA" 7200
+                          "GS" -7200
+                          "SS" 7200
+                          "ES" 3600
+                          "LK" 19800
+                          "SD" 7200
+                          "SR" -10800
+                          "SJ" 3600
+                          "SZ" 7200
+                          "SE" 3600
+                          "CH" 3600
+                          "SY" 10800
+                          "TW" 28800
+                          "TJ" 18000
+                          "TZ" 10800
+                          "TH" 25200
+                          "TL" 32400
+                          "TG" 0
+                          "TK" 46800
+                          "TO" 46800
+                          "TT" -14400
+                          "TN" 3600
+                          "TR" 10800
+                          "TM" 18000
+                          "TC" -18000
+                          "TV" 43200
+                          "UG" 10800
+                          "UA" 7200
+                          "AE" 14400
+                          "GB" 0
+                          "US" -21600
+                          "UM" 43200
+                          "UY" -10800
+                          "UZ" 18000
+                          "VU" 39600
+                          "VE" -14400
+                          "VN" 25200
+                          "VG" -14400
+                          "VI" -14400
+                          "WF" 43200
+                          "YE" 10800
+                          "ZM" 7200
+                          "ZW" 7200
+                          "AX" 7200})
+
 (def with-code 
   (memoize (fn [code]
              (try (let [id (country-id code)
-                        name (country-name code)]
-                    (Country. id code name))
+                        name (country-name code)
+                        delta-s (country-tz code)]
+                    (Country. id code name delta-s))
                   (catch :default err
                     (println (str "ERROR trying to get country data for: " code) 
                              err)
